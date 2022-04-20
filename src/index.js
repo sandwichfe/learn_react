327d0ff17fe8d6208fe49dc0142638e8effb6ec4
sandwichfe <1344382941@qq.com>
2022-04-20 14:47:45 +0800

    完善

--- src/index.js
+++ src/index.js
@@ -2,33 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
+// class Square extends React.Component {
 
-  constructor(props){
-      super(props);
-      this.state = {
-        value:null
-      }
-  }
+//   constructor(props){
+//       super(props);
+//       this.state = {
+//         value:null
+//       }
+//   }
 
 
-    render() {
-      return (
-        <button className="square" 
-        onClick={()=>{this.setState({value:'X'})}}>
-          {this.state.value}
-        </button>
-      );
-    }
-  }
+//     render() {
+//       return (
+//         <button className="square" 
+//         onClick={()=>this.props.onClick()}>
+//           {this.props.value}
+//         </button>
+//       );
+//     }
+//   }
+
+// 上面可直接写成这样...
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
   
   class Board extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-          squares: Array(9).fill(null)
-        };
-    }
 
 
     renderSquare(i) {
@@ -36,17 +39,14 @@ class Square extends React.Component {
        * 为了提高可读性，我们把返回的 React 元素拆分成了多行，
        * 同时在最外层加了小括号，这样 JavaScript 解析的时候就不会在 return 的后面自动插入一个分号从而破坏代码结构了。
        */
-      return  (<Square value = {this.state.squares[i]} 
-      onClick= { ()=>this.handleClick(i)}/>
+      return  (<Square value = {this.props.squares[i]} 
+      onClick= { ()=>this.props.onClick(i)}/>
       );
     }
   
     render() {
-      const status = 'Next player: X';
-  
       return (
         <div>
-          <div className="status">{status}</div>
           <div className="board-row">
             {this.renderSquare(0)}
             {this.renderSquare(1)}
@@ -68,20 +68,110 @@ class Square extends React.Component {
   }
   
   class Game extends React.Component {
+    constructor(props){
+       super(props);
+       this.state = {
+         history:[{
+           squares:Array(9).fill(null),
+         }],
+         stepNumber: 0,
+         xIsNext:true
+       }
+    }
+
+    handleClick(i){
+      // 数组.slice 对数组进行拷贝
+      const history = this.state.history.slice(0, this.state.stepNumber + 1);
+      const current = history[history.length - 1];
+      const squares = current.squares.slice();
+      // 如果已经获胜或者此格子已经填过值 就不做操作
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
+      squares[i] = this.state.xIsNext?'X':'O';
+      this.setState({
+        history: history.concat([{
+          squares: squares,
+        }]),
+        stepNumber: history.length,
+        xIsNext: !this.state.xIsNext,
+      });
+    }
+
+    jumpTo(step) {
+      this.setState({
+        stepNumber: step,
+        xIsNext: (step % 2) === 0,
+      });
+    }
+
     render() {
+      const history = this.state.history;
+      const current = history[this.state.stepNumber];
+      const winner = calculateWinner(current.squares);
+
+      const moves = history.map((step, move) => {
+        const desc = move ?
+          'Go to move #' + move :
+          'Go to game start';
+        return (
+          <li key={move}>
+            <button onClick={() => this.jumpTo(move)}>{desc}</button>
+          </li>
+        );
+      });  
+
+
+      let status;
+      if (winner) {
+        status = 'Winner: ' + winner;
+      } else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
+  
       return (
         <div className="game">
           <div className="game-board">
-            <Board />
+            <Board
+              squares={current.squares}
+              onClick={(i) => this.handleClick(i)}
+            />
           </div>
           <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
+            <div>{status}</div>
+            <ol>{moves}</ol>
           </div>
         </div>
       );
     }
   }
+
+
+  /**
+   * 判断出获胜者 
+   * @param {} squares  传入长度为 9 的数组
+   * @returns  “X”，“O” 或 “null”
+   */
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
   
   // ========================================
   
